f887368db39043a816dfc72562379dfb32163b78
yesinternet <dev.yesinternet@gmail.com>
2015-06-08 15:48:08 +0300

    Logo

--- templates/gk_news2/js/sp.js
+++ templates/gk_news2/js/sp.js
@@ -3,7 +3,12 @@ jQuery.noConflict();
 
 jQuery(document).ready(function() {
 
-//Move Item Gallery after extra fields 
+    //Move Item Gallery after extra fields 
+    jQuery(".sigFreeContainer").detach().insertAfter('.itemExtraFields');
+    
+});
 
-jQuery(".sigFreeContainer").detach().insertAfter('.itemExtraFields');
+jQuery(window).scroll(function() {
+        jQuery('#gkLogoSmall>img').attr('src','images/logo/logo-fixedMenu.png');
+
 });
